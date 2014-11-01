338cf904cc1da907904d9a350d1ce6c9262e1b92
Arley Prates <arleyprates@gmail.com>
2014-11-01 20:55:49 -0300

    validacao de usuario

--- valida.php
+++ valida.php
@@ -8,21 +8,14 @@ $password = $_POST['password'];
 
 $sql = mysql_query("SELECT PNOME, UNOME, SENHA FROM usuario WHERE CPF = $cpf");
 $result = mysql_fetch_array($sql);
-if ($password = $result[2]);
-	mysql_free_result($sql);
-	$row = mysql_affected_rows();
-	echo $row;
-	$sql = mysql_query("SELECT cpf_mod FROM moderador WHERE CPF_mod = $cpf");
-	$row = mysql_affected_rows();
-	echo $row;
-	if ($row > 0)
-		echo $row;
-	//if ($sql) {
-	//	$result = mysql_fetch_array($sql);
-	//	echo $result[0];
-	//	echo "moderador!";
-	//}
-
+	if ($password == $result[2]){
+		echo $result[2];
+		$sqlmodera = mysql_query("SELECT cpf_mod FROM moderador WHERE CPF_mod = $cpf");
+		$result = mysql_fetch_array($sqlmodera);
+		if ($cpf == $result[0]) {
+			header("location: moderacao.php");
+		}
+	}
 	/*
 	Moderador: SELECT COD_SETOR FROM MODERADOR, MODERA WHERE CPF_MOD=cpf AND CPF_MOD=CPF_USUARIO;
 	
