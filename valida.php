76ab4b8731727d7633e06fd7b19260a813ada865
Arley Prates <arleyprates@gmail.com>
2014-11-01 19:38:42 -0300

    teste sessao moderacao

--- valida.php
+++ valida.php
@@ -7,13 +7,22 @@ $cpf = $_POST['cpf'];
 $password = $_POST['password'];
 
 $sql = mysql_query("SELECT PNOME, UNOME, SENHA FROM usuario WHERE CPF = $cpf");
-
 $result = mysql_fetch_array($sql);
 if ($password = $result[2]);
-	echo $result[2];
+	mysql_free_result($sql);
+	$row = mysql_affected_rows();
+	echo $row;
+	$sql = mysql_query("SELECT cpf_mod FROM moderador WHERE CPF_mod = $cpf");
+	$row = mysql_affected_rows();
+	echo $row;
+	if ($row > 0)
+		echo $row;
+	//if ($sql) {
+	//	$result = mysql_fetch_array($sql);
+	//	echo $result[0];
+	//	echo "moderador!";
+	//}
 
-
-	
 	/*
 	Moderador: SELECT COD_SETOR FROM MODERADOR, MODERA WHERE CPF_MOD=cpf AND CPF_MOD=CPF_USUARIO;
 	
