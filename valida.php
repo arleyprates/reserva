81daafea5ad612033a3dc5e1ba4b57b70a01f97b
Arley Prates <arleyprates@gmail.com>
2014-11-02 14:47:43 -0300

    valida com sessao e tipo de usuario

--- valida.php
+++ valida.php
@@ -7,29 +7,42 @@ $password = $_POST['password'];
 
 $sql = mysql_query("SELECT PNOME, UNOME, SENHA FROM usuario WHERE CPF = $cpf");
 $result = mysql_fetch_array($sql);
+	$nome = $result[0];
 	if ($password == $result[2]){
 		$sqladm = mysql_query("SELECT cpf_adm FROM moderador WHERE cpf_adm = $cpf");
 		$result = mysql_fetch_array($sqladm);
 		if ($cpf == $result[0]){
-			header("location: adm.php");
+			session_start();
+			$_SESSION['user'] = $nome;
+			$_SESSION['tipousuario'] = 0; 
+			header("location: administrador/index.php");
 			include("close_conexao.php");
 			exit;
 		}
 		$sqlmodera = mysql_query("SELECT cpf_mod FROM moderador WHERE CPF_mod = $cpf");
 		$result = mysql_fetch_array($sqlmodera);
 		if ($cpf == $result[0]){
-			header("location: moderacao.php");
+			session_start();
+			$_SESSION['user'] = $nome;
+			$_SESSION['tipousuario'] = 1;
+			header("location: administrador/index.php");
 			include("close_conexao.php");
 			exit;
 		}
 		$sqlprofessor = mysql_query("SELECT cpf_prof FROM professor WHERE CPf_prof = $cpf");
 		$result = mysql_fetch_array($sqlprofessor);
 		if ($cpf == $result[0]){
-			header("location: professor.php");
+			session_start();
+			$_SESSION['user'] = $nome;
+			$_SESSION['tipousuario'] = 2;
+			header("location: professor/index.php");
 			include("close_conexao.php");
 			exit;
 		}
-		header("location: user.php");
+		session_start();
+		$_SESSION['user'] = $nome;
+		$_SESSION['tipousuario'] = 3;
+		header("location: usuario/index.php");
 		exit;
 	}else {
 		echo "<script>alert('Usuario e Senha Invalida(o)');
