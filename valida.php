8bc4fd1e259077b1cf17244b4ddc00b91484444c
Arley Prates <arleyprates@gmail.com>
2014-11-02 02:16:19 -0300

    validacao ainda precisa de testes

--- valida.php
+++ valida.php
@@ -1,5 +1,4 @@
 <?php
-
 include("conexao.php");
 include("erro.php");
 
@@ -9,21 +8,26 @@ $password = $_POST['password'];
 $sql = mysql_query("SELECT PNOME, UNOME, SENHA FROM usuario WHERE CPF = $cpf");
 $result = mysql_fetch_array($sql);
 	if ($password == $result[2]){
-		echo $result[2];
 		$sqlmodera = mysql_query("SELECT cpf_mod FROM moderador WHERE CPF_mod = $cpf");
 		$result = mysql_fetch_array($sqlmodera);
-		if ($cpf == $result[0]) {
+		if ($cpf == $result[0]){
 			header("location: moderacao.php");
 		}
+		$sqlprofessor = mysql_query("SELECT cpf_prof FROM professor WHERE CPf_prof = $cpf");
+		$result = mysql_fetch_array($sqlprofessor);
+		if ($cpf == $result[0]){
+			header("location: professor.php");
+		}
+		$sqladm = mysql_query("SELECT cpf_adm FROM moderador WHERE cpf_adm = $cpf");
+		$result = mysql_fetch_array($sqladm);
+		if ($cpf == $result[0]){
+			header("location: adm.php");
+		}
+		//header("location: user.php");
+	}else {
+		echo "<script>alert('Usuario e Senha Invalida(o)');
+		window.location.replace('login.html');
+		</script>";
 	}
-	/*
-	Moderador: SELECT COD_SETOR FROM MODERADOR, MODERA WHERE CPF_MOD=cpf AND CPF_MOD=CPF_USUARIO;
-	
-	Professor: SELECT SIAP, COD_DEPTO FROM PROFESSOR WHERE CPF_PROF=cpf;
-	
-	Administrador: SELECT COD_SETOR FROM MODERADOR JOIN MODERA ON CPF_MOD=CPF_USUARIO WHERE CPF_ADM=cpf;
-	*/
-
 include("close_conexao.php");
-
 ?>
