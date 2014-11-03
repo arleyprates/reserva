daed2f62d4f2acd4d83fbd7658a8eaf273a9a1b0
guest <guest@graco01>
2014-11-03 18:32:12 -0300

    reservar a sala

---
+++ reservar.php
@@ -0,0 +1,14 @@
+<?php
+ $cpf = $_SESSION["cpf"];
+ $email = $_SESSION["email"];
+ $data = $_GET["data"];
+ $horai =$_GET["horai"];
+ $sala = $_GET["sala"];
+
+ 
+ if($_SESSION['tipousuario'] != 1){
+
+ }
+
+
+?>
