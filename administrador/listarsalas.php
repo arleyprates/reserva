646d8b3267cda90cb770031c2a6d1eaaea611036
w2 <nesskraven@gmail.com>
2014-11-03 14:09:08 -0200

    Listagem de salas funcionando, porem incompleto

--- administrador/listarsalas.php
+++ administrador/listarsalas.php
@@ -1,70 +1,69 @@
 <?php
 
-function tipoBusca()
-{
-	if(strcmp($tipo, "Numero"))
+include("../conexao.php");
+
+header('Content-type: text/html; charset=UTF-8'); 
+
+$busca = $_GET['buscarsala'];
+$tipo = $_GET['criterio'];
+
+
+	if(strcmp($tipo, "Numero") == 0)
 	{
-		$sql = mysql_query("SELECT nr_sala, capacidade, id_tipo_sala FROM sala WHERE nr_sala = $nr");
-		$result = mysql_fetch_array($sql);
+		$sql = mysql_query("SELECT * FROM sala WHERE sala.nr_sala = $busca");
+	    $result = mysql_fetch_array($sql);
 		if($result)
 		{
-			echo "$result[0]";
+            echo "Número da sala: $result[0]";
 			echo "</br>";
-			echo "$result[1]";
+			echo "Capacidade: $result[1] alunos";
 			echo "</br>";
-			echo "$result[2]";
+			echo "Tipo de Sala: $result[2]";
 			echo "</br>";
 		}
+        else
+        {
+            mysql_error();
+        }
 	}
 
-	else if(strcmp($tipo, "Setor"))
+	else if(strcmp($tipo, "Setor") == 0)
 	{
-		$sql = mysql_query("SELECT nr_sala, capacidade, id_tipo_sala FROM sala" );
-		while ($result = mysql_fetch_array($sql))
+
+		$sql = mysql_query("SELECT * FROM gerencia LEFT JOIN (sala, setor) on (gerencia.id_sala = sala.nr_sala and gerencia.cod_setorg = setor.cod_setor)" );
+		while ($row = mysql_fetch_array($sql))
 	{
-    	if(strcmp($result['descrição'], $tipo))
+        if(strcmp($row['nome'], utf8_decode($busca)) == 0) //problema aqui!!
     	{	
-    		echo $result['descrição'];
-    		echo $result['nr_sala'];
-    		echo $result['capacidade'];
+    		//echo $row[6];
+            echo "</br>";
+            echo "Nome do Setor: ". $row['nome'];
+            echo "</br>";
+    		echo "Número da sala: ". $row['nr_sala'];
+            echo "</br>";
+    		echo "Capacidade de ". $row['capacidade'] . " alunos";
+            echo "</br>";
     	 }
 	   }
 	}
-	else if(strcmp($tipo, "Tipo de sala"))
+	else if(strcmp(utf8_decode($tipo), "Tipo de sala") == 0)
 	{
 		$sql = mysql_query("SELECT nr_sala, capacidade, id_tipo_sala FROM sala" );
 		while ($result = mysql_fetch_array($sql))
 	{
-    	if(strcmp($result['descrição'], $tipo))
+    	if(strcmp($result['descrição'], utf8_decode($busca)))
     	{	
-    		echo $result['descrição'];
-    		echo $result['nr_sala'];
-    		echo $result['capacidade'];
+    		echo "</br>";
+            echo $result['descrição'];
+    		echo "</br>";
+            echo $result['nr_sala'];
+    		echo "</br>";
+            echo $result['capacidade'];
+            echo "</br>";
     	 }
 	   }	
 	}
-}
+
 
 ?>
 
-echo "Nome: ". $row['nome'];
-    echo  "<br>";
-    echo "CPF/CNPJ: ". $row['cpfcnpj'];
-    echo "<br>";
-    echo "Natureza: " . $row['natureza'];
-    echo "<br>";
-    echo "Endereco: " . $row['endereco'];
-    echo "<br>";
-    echo "Estado: ". $row['estado'];
-    echo "<br>";
-    echo "DDD: ". $row['ddd'];
-    echo "<br>";
-    echo "Telefone: ". $row['telefone'];
-    echo "<br>";
-    echo "Licitacao escolhida: ". $row['licitacao'];
-    echo "<br>";
-    echo "Data: ". $row['data'];
-    echo "<br>";
-    echo "Hora: ". $row['hora'];
-    echo "<br>";
-    echo "<br>";
--- administrador/removesala.php
+++ administrador/removesala.php
@@ -3,18 +3,9 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type: content=text/html; charset=UTF-8" />
+<!-- <meta http-equiv="Content-Type: content=text/html; charset=UTF-8" /> -->
 <title>Cadastro de usuários</title>
 
-<!--
-<style type="text/css">
-.style1 {
-  color: #FF0000;
-  font-size: x-small;
-}
-.style3 {color: #0000FF; font-size: x-small; }
-</style>
--->
 <link href="../css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
@@ -22,6 +13,7 @@
   <h1 style="color:#FFFFFF">Remoção de Sala</h1>
   <!--<p style="color:#FFFFFF"></p> -->
 </div>
+<form action="listarsalas.php" method="get">
 <tr>
       <td width="69">Dado da sala:</td>
       <td width="546"><input name="buscarsala" type="text" id="buscarsala" size="20" maxlength="60" onclick="" />
@@ -33,9 +25,10 @@
          </select></p></p>
   </td>
       
-       <button>Pesquisar</button>
+       <input name="confirmar"  type="submit" id="confirm" value="Confirmar" />
 
 </tr>
+</form>
 
 </body>
 </html>
