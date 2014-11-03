3eec2821aec7d3a35497309d0387f3b55ad1b97c
w2 <nesskraven@gmail.com>
2014-11-03 19:30:26 -0200

    Reservas consertada

--- reservas.php
+++ reservas.php
@@ -1,62 +1,65 @@
 <?php
-include ("conexao.php");
+include("conexao.php");
 $gmtDate = gmdate("D, d M Y H:i:s");
 header("Expires: {$gmtDate} GMT");
 header("Last-Modified: {$gmtDate} GMT");
 header("Cache-Control: no-cache, must-revalidade");
 header("Pragma: no-cache");
-$data = $_GET["data"];
-$codsetor = $_GET["setor"];
-$res = mysqli_query($conexao,"SELECT horario_abertura, horario_encerramento FROM setor WHERE cod_setor=$codsetor");
-$total = mysqli_num_rows($res);
+$data = date("Y-m-d");  /* =$_GET["data"] */
+
+
+//$codsetor = $_GET["setor"];
+$codsetor = 1; 
+$res = mysql_query("SELECT `horario_abertura`, `horario_encerramento` FROM `setor` WHERE `cod_setor`='$codsetor'");
+//$total = mysqli_num_rows($res);
 $encerramento;
 $abertura;
-for($i=0;$i<$total;$i++){
-	$dados = mysqli_fetch_row($res);
+///for($i=0;$i<$total;i++){
+	while($dados = mysql_fetch_array($res))
+	{
     $abertura = $dados[0];
     $encerramento = $dados[1];
                 
-}
+	}
 $a = explode(":", $abertura);
 $e = explode(":", $encerramento);
 
-$sala;
-$res = mysqli_query($conexao,"SELECT id_sala FROM gerencia WHERE cod_setorg=$codsetor");
-$total = mysqli_num_rows($res);
-for($i=0;$i<$total;$i++){
-	$sala = mysqli_fetch_row($res);
-                   
+
+$res = mysql_query("SELECT `id_sala` FROM `gerencia` WHERE `cod_setorg`='$codsetor'");
+//$total = mysqli_num_rows($res);
+
+while($dados =  mysql_fetch_array($res))
+{
+	$sala[] = $dados[0];
+	//echo "</br>";
 }
-$tabela;
+
 for($i=$a[0];$i<$e[0];$i++){
 	foreach ($sala as $s) {
 		$tabela[$i][$s]="livre";
 	}
 }
+
 foreach ($sala as $s) {
 	
-	$res = mysqli_query($conexao,"SELECT horario_inicio , horario_fim, pnome FROM  reserva, usuario WHERE data_reserva='$data' AND num_sala=$s AND cpf_usr=cpf ORDER BY horario_inicio");
-	$total = mysqli_num_rows($res);
-	for($i=0;$i<$total;$i++){
-		$dados = mysqli_fetch_row($res);
+	$res = mysql_query("SELECT `horario_inicio` , `horario_fim`, `pnome` FROM `reserva`, `usuario` WHERE `data_reserva`='$data' AND `num_sala`='$s' AND `cpf_usr`=`cpf` ORDER BY `horario_inicio`");
+	//$total = mysqli_num_rows($res);
+	while($dados = mysql_fetch_array($res)){
 		$inicio= explode(":", $dados[0]);
 		$fim= explode(":", $dados[1]);
 		$username=$dados[2];
-		$dif=$fim-$inicio;
-		$aux=$inicio;
-		while($dif>0){
-			$tabela[$aux][$s]=$username;
-			$aux++;
-			$dif--;
-
-		}
-
 	}
 }
+
 echo "<table class=\"table\">";
 echo " <thead>";
 echo "  <tr>";
 echo "   <th>Hora</th>";
+
+echo $sala[0];
+echo $sala[1];
+echo $sala[3];
+
 foreach ($sala as $s) {
 	echo "   <th>sala $s</th>";
 }
@@ -79,3 +82,4 @@ for($i=$a[0];$i<$e[0];$i++){
 echo " </tbody>";
 echo "</table>";
 ?>
+
