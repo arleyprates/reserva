66e1ecee67d2c2eb94e4f57fbd0132beb97ab4fe
madson <madson@madson>
2014-11-04 12:06:02 -0300

    diferenciar reserva com ou sem moderaçao

--- reservar.php
+++ reservar.php
@@ -16,6 +16,7 @@ $repetir="semana";
 $data= '2014-11-03';
 $codsetor="1";
 $sala="4";
+$tipousario="2";
 //obter horario de encerramento do setor
 //$res= mysql_query("SELECT horario_encerramento FROM setor WHERE cod_setor='$codsetor'");
 //$horae = mysql_fetch_array($res);
@@ -40,31 +41,34 @@ echo " <option value=\"1\">Diaria</option>";
 echo " <option value=\"2\">Semanal</option>";
 echo "</selection>";
 echo "</p>";
-//$timestamp= strtotime($data);
-if($repetir=="dia"){
-	$i=0;
-	$dia= date("w",strtotime($data."+$i days"));
-	while(($dia!=6) AND ($dia!=0)){
-		$dt= date("Y-m-d",strtotime($data."+$i days"));
-		$res = mysql_query("INSERT INTO resera VALUES('$cpf,'$sala','$dt',$horai,$horaf,'null')");
-		$i++;
+//insereir reserva na base
+if($tipousuario!= "1"){
+	if($repetir=="dia"){
+		$i=0;
 		$dia= date("w",strtotime($data."+$i days"));
+		while(($dia!=6) AND ($dia!=0)){
+			$dt= date("Y-m-d",strtotime($data."+$i days"));
+			$res = mysql_query("INSERT INTO resera VALUES('$cpf,'$sala','$dt',$horai,$horaf,'null')");
+			$i++;
+			$dia= date("w",strtotime($data."+$i days"));
+		}
+	}else{
+		if($repetir=="semana"){
+			$nsemana=4;
+	        	$i=0;
+        		while($i<$nsemana){
+	                	$dt= date("Y-m-d",strtotime($data."+$i weeks"));
+				$res = mysql_query("INSERT INTO resera VALUES('$cpf,'$sala','$dt',$horai,$horaf,'null')");
+                		$i++;
+                	
+        		}
+
+		}else{
+                       	$res = mysql_query("INSERT INTO resera VALUES('$cpf,'$sala','$data',$horai,$horaf,'null')"); 
+                	}
+		}
 	}
 }else{
-	if($repetir=="semana"){
-		$nsemana=4;
-	        $i=0;
-        	while($i<$nsemana){
-                	$dt= date("Y-m-d",strtotime($data."+$i weeks"));
-			$res = mysql_query("INSERT INTO resera VALUES('$cpf,'$sala','$dt',$horai,$horaf,'null')");
-                	$i++;
-                	
-        	}
 
-	}else{
-                       $res = mysql_query("INSERT INTO resera VALUES('$cpf,'$sala','$data',$horai,$horaf,'null')"); 
-                }
-	}
 }
-
 ?>
