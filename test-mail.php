53f8dccc1486c88a6a38f1f87fd1b0db4654741c
Arley Prates <arleyprates@gmail.com>
2014-11-04 15:42:00 -0300

    ajuste email falta tratar array

--- test-mail.php
+++ test-mail.php
@@ -5,28 +5,41 @@
 // -> select email de user de mesmo cpf do moderador
 //
 require("conexao.php");
-$idsalassolicitada= '10';
-
+$idsalassolicitada=10;
+/*
 $query1= mysql_query ("SELECT cod_setorg FROM gerencia WHERE id_sala=$idsalassolicitada");
 $codigosetor= mysql_fetch_array($query1);
-//print_r($codigosetor);
+print_r($codigosetor);
 
-$query2= mysql_query("SELECT cpf_usuario FROM modera WHERE cod_setor='1'"); 
-//IN (SELECT cod_setorg FROM gerencia WHERE id_sala=$idsalassolicitada)");
+
+$query2= mysql_query("SELECT cpf_usuario FROM modera WHERE cod_setor IN (SELECT cod_setorg FROM gerencia WHERE id_sala=$idsalassolicitada)");
 
 while ($moderadoressetor= mysql_fetch_array($query2)){
 $array[]=$moderadoressetor;
-//print_r($moderadoressetor);
 }
 print_r($array);
-$query3= mysql_query("SELECT email FROM usuario WHERE cpf IN (SELECT cpf_usuario FROM modera WHERE cod_setor=(SELECT cod_setorg FROM gerencia WHERE id_sala=$idsalassolicitada))");
-$emailsmod= mysql_fetch_array($query3);
-//print_r($emailsmod);
+*/
 
+$query3= mysql_query("SELECT email FROM usuario WHERE cpf IN (SELECT cpf_usuario FROM modera WHERE cod_setor=(SELECT cod_setorg FROM gerencia WHERE id_sala=$idsalassolicitada))");
+#$query3= mysql_query("SELECT email FROM usuario WHERE cpf IN (SELECT cpf_usuario FROM modera WHERE cod_setor=(SELECT cod_setorg FROM gerencia WHERE id_sala=$idsalassolicitada))");
+
+$var=array();
+while ($resultado[]= mysql_fetch_array($query3)){
+$var[]= $resultado;
+}
+print_r($resultado);
 /*
+while ($emailsmod= mysql_fetch_array($query3)){
+$array[]=$emailsmod;
+echo $array;
+}
+*/
+//print_r($array);
+
+
 $to      = 'brsdcc@gmail.com';
 $subject = 'TEST php';
-$message = 'hello test php';
+$message = 'hello test php '$resultado'';
 $headers = 'From: brsdcc@gmail.com' . "\r\n" .
     'Reply-To: brsdcc@gmail.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
