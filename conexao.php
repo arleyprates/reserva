e50c3be5297ff028a75cd6a41b439a62818418b3
Arley Prates <arleyprates@gmail.com>
2014-11-04 02:44:04 -0300

    teste.php
    ajuste string incorretas

--- conexao.php
+++ conexao.php
@@ -9,7 +9,6 @@ $senha = '123';
 $banco = 'siges';
 
 if($conexao = mysql_connect($servidor, $usuario, $senha)){
-	echo "Conexao feita com sucesso!!";
 	mysql_select_db($banco, $conexao);
 }else {
 	echo "Nao foi possivel conectar com o servidor";
