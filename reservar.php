16713f22fca257e7a56ada4dc9896097b76b880a
madson <madson@madson>
2014-11-04 11:50:22 -0300

    insert reserva sem repetiçao

--- reservar.php
+++ reservar.php
@@ -15,6 +15,7 @@ $horai='09:00:00';
 $repetir="semana";
 $data= '2014-11-03';
 $codsetor="1";
+$sala="4";
 //obter horario de encerramento do setor
 //$res= mysql_query("SELECT horario_encerramento FROM setor WHERE cod_setor='$codsetor'");
 //$horae = mysql_fetch_array($res);
@@ -22,12 +23,23 @@ $horae='20:00:00';
 //selection da hora final
 $hi=explode(":",$horai);
 $hf=explode(":",$horae);
+echo "<p>";
 echo "Hora Final: ";
 echo "<selection id=\"horafinal\" name=\"horaf\">";
 for($i=$hi[0];$i<$hf;$i++){
 	echo " <option value=\"$i\">$i</option>";
 }
 echo "</selection>";
+echo "</p>";
+//selction da repticao
+echo "<p>";
+echo "Tipo de repeticao: ";
+echo "<selection id=\"repetir\" name=\"repet\">";
+echo " <option value=\"0\">Nao</option>";
+echo " <option value=\"1\">Diaria</option>";
+echo " <option value=\"2\">Semanal</option>";
+echo "</selection>";
+echo "</p>";
 //$timestamp= strtotime($data);
 if($repetir=="dia"){
 	$i=0;
@@ -48,14 +60,9 @@ if($repetir=="dia"){
         	}
 
 	}else{
-		$nmes=4;
-                $i=0;
-                while($i<$nmes){
-			$m=$i*5;
-                        echo date("Y-m-d",strtotime($data."+$m weeks "));
-                        $i++;
-                        
+                       $res = mysql_query("INSERT INTO resera VALUES('$cpf,'$sala','$data',$horai,$horaf,'null')"); 
                 }
 	}
 }
+
 ?>
